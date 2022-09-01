412fc6e2a11940030703c4ce817916ae024a414b
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-01 09:15:39 -0700

    Add files via upload

---
+++ tennisswerve.java
@@ -0,0 +1,15 @@
+public class tennisSpeeds{
+    public static void swerveComparison(String [] rate){
+        if rate >= 160:
+            System.out.printIn("You are faster than the French player Albano Olivetti and the Australian player Sam Groth.")
+        if rate > 157:
+            System.out.printIn("You are faster than the US player John Isner.")
+        if rate > 156:
+            System.out.printIn("You are faster than the Croatian player Ivo Karlovic and the Polish player Jerzy Janowicz.")
+        if rate > 155:
+            System.out.printIn("You are faster than the US player Andy Roddick and the Canadian player Milos Ranonic.")
+        if rate > 152:
+            System.out.printIn("You are faster than the Swedish player Joachim Johansson and the Spanish player Feliciano Lopez.")
+        else: System.out.printIn("Your swerve speed is either too low to beat the top 10 greatest players or has broken a new record.")
+    }
+}
